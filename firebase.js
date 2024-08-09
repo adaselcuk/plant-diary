f896aea59f87836c22fab80f745b9003dd05828c
Ada <adaselcuk16@gmail.com>
2024-08-09 21:57:27 +0300

    html for sign up and sign in

--- firebase.js
+++ firebase.js
@@ -21,5 +21,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { auth };
 
 export default app;
