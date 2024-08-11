10e992d5665f4a12c87ebdd44ba01b80324dce98
Ada <adaselcuk16@gmail.com>
2024-08-12 00:27:34 +0300

    firebase changes - working on this

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,6 +22,48 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+
+// for signing up
+document.getElementById('submitSignUp').addEventListener('click', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('rEmail').value;
+  const password = document.getElementById('rPassword').value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+      document.getElementById('signUpMessage').innerHTML = 'Registered Successfully!';
+      document.getElementById('signUpMessage').style.display = 'block';
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      document.getElementById('signUpMessage').innerHTML = errorMessage;
+      document.getElementById('signUpMessage').style.display = 'block';
+    });
+});
+
+// for signing in
+document.getElementById('submitSignIn').addEventListener('click', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+      document.getElementById('signInMessage').innerHTML = 'Logged In Successfully!';
+      document.getElementById('signInMessage').style.display = 'block';
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      document.getElementById('signInMessage').innerHTML = errorMessage;
+      document.getElementById('signInMessage').style.display = 'block';
+    });
+});
 
 export { auth };
 
